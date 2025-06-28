7fea53d50540881bbd4dae980066e03e2b011ed8
Ana María Cárdenas Gasca <am.cardenas927@gmail.com>
2025-06-28 16:58:39 -0700

    Try new deploy strategy

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,49 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.ts', // Change from .js to .ts
-  resolve: {
-    extensions: [".ts", ".js"], // Add TypeScript extensions
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: argv.mode || 'development',
+    entry: './src/index.ts',
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'), 
+      clean: true,
+      publicPath: isProduction ? '/radford_map/' : '/',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
     ],
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: __dirname + '/dist',
-    clean: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  performance: {
-    hints: false,
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        babylon: {
-          test: /[\\/]node_modules[\\/]@babylonjs[\\/]/,
-          name: 'babylon',
-          chunks: 'all',
+    performance: {
+      hints: false,
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          babylon: {
+            test: /[\\/]node_modules[\\/]@babylonjs[\\/]/,
+            name: 'babylon',
+            chunks: 'all',
+          }
         }
       }
     }
-  }
+  };
 };
